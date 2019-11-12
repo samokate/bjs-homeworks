ae283bc2d2416177eaa5cf4a8998d27862ddabc0
samokate <edsamoylova@gmail.com>
2019-11-12 15:31:12 +0300

    marks.slice(0, 5), return averageMark corrected

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -34,7 +34,7 @@ function getAverageMark(marks){
 
     if (marks.length > 5) {
         console.log('Больше пяти оценок. Средний балл выведен на базе первых пяти оценок.');
-        marksArr = marks.slice(0, 4);
+        marksArr = marks.slice(0, 5);
     } else {
         marksArr = marks;
     }
@@ -45,9 +45,7 @@ function getAverageMark(marks){
             marksSum += mark;
     }
 
-    let averageMark = marksSum / marksArr.length;
-
-    return averageMark;
+    return averageMark = marksSum / marksArr.length;
 }
 
 function calculateDrinkTask(){

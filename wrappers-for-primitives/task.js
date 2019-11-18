adaafd5f51f00cbe0faa6d0935b3188621278e8b
samokate <edsamoylova@gmail.com>
2019-11-18 11:14:38 +0300

    Problems with both tasks

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,10 +12,49 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    
+    const obj = {
+        percent, 
+        contribution, 
+        amount,
+        date   // в браузере выводит неправильное значение date
+       }
+         
+   for (let prop in obj) {  
+    if (isNaN(Number(obj[prop]))) {
+       return `${prop} содержит неправильное значение ${obj[prop]}` 
+     }
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+
+
+    let today = new Date(); //задаём сегодняшнюю дату
+    let dateX = new Date(date);
+    let months = dateX.getMonth() - today.getMonth() + (12 * ( dateX.getFullYear() - today.getFullYear() ));
+
+    let payToBank = amount - contribution;
+    let percentPerMonth = percent / 100 / 12;
+    let paymentPerMonth = payToBank * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth) ** date) - 1));
+
+    let totalAmount = paymentPerMonth * months;
+
+    return totalAmount.toFixed(2);
+
 }
+
+// tests
+
+console.log(calculateTotalMortgage('10',0,'50000',12))         // expected => 52749.53
+console.log(calculateTotalMortgage(10,1000,50000,12))        // expected => 51694.54
+console.log(calculateTotalMortgage(10,0,20000,24))       // expected => 22149.56
+console.log(calculateTotalMortgage(10,1000,20000,24))       // expected => 21042.09
+console.log(calculateTotalMortgage(10,20000,20000,24))        // expected => 0
+console.log(calculateTotalMortgage(10,0,10000,36))       // expected => 11616.19
+console.log(calculateTotalMortgage(15,0,10000,36))       // expected => 12479.52
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +64,16 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+
+    /* if (name === " " || name === 'null' || name === 'undefined' || name === '""') {  // почему не работает этот способ?
+        name = 'Аноним'; 
+    } */
+
+   if (typeof name !== 'string') {  // почему не работает typeof?
+        name = "Аноним";
+    }
+    
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
 }
+

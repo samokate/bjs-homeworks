699657c12ba3b942b250599b81d9bb113a284123
samokate <edsamoylova@gmail.com>
2019-11-25 11:56:56 +0300

    Переименовала переменные Amount и Contribution, а также числовые параметры  заменила на строки

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -14,8 +14,8 @@ function calculateMortgage() {
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
     let percentPerMonth;  // если задаю const , в консоли ошибка Uncaught SyntaxError: Missing initializer in const declaration
-    let Contribution;
-    let Amount;
+    let contributionNew;
+    let amountNew;
     
     if (parseFloat(percent)) {
         percentPerMonth = parseFloat(percent / 100 / 12);
@@ -24,13 +24,13 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     }
 
     if (parseInt(contribution) || contribution == 0) {
-        Contribution = (parseInt(contribution)) ? parseInt(contribution) : 0;
+        contributionNew = (parseInt(contribution)) ? parseInt(contribution) : 0;
     } else {
         return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
     }
 
     if (parseInt(amount)) {
-        Amount = parseInt(amount);
+        amountNew = parseInt(amount);
     } else {
         return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
     }
@@ -39,8 +39,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let today = new Date(); //задаём сегодняшнюю дату
     let dateX = new Date(date);
     let months = parseInt((dateX - today) / (1000 * 3600 * 24 * 30));
-
-    let payToBank = Amount - Contribution;
+    let payToBank = amountNew - contributionNew;
     let paymentPerMonth = payToBank * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth) ** months) - 1));
 
     let totalAmount = paymentPerMonth * months;
@@ -53,13 +52,13 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
 // tests - консоль выводит неадекватные результаты. Но, при вводе нижеуказанных данных в поля, результаты верные. Почему?
 
-console.log(calculateTotalMortgage('10',0,'50000',12))         // expected => 52749.53   
-console.log(calculateTotalMortgage(10,1000,50000,12))        // expected => 51694.54
-console.log(calculateTotalMortgage(10,0,20000,24))       // expected => 22149.56
-console.log(calculateTotalMortgage(10,1000,20000,24))       // expected => 21042.09
-console.log(calculateTotalMortgage(10,20000,20000,24))        // expected => 0
-console.log(calculateTotalMortgage(10,0,10000,36))       // expected => 11616.19
-console.log(calculateTotalMortgage(15,0,10000,36))       // expected => 12479.52
+console.log(calculateTotalMortgage('10','0','50000','12'))         // expected => 52749.53   
+console.log(calculateTotalMortgage('10','1000','50000','12'))        // expected => 51694.54
+console.log(calculateTotalMortgage('10','0','20000','24'))       // expected => 22149.56
+console.log(calculateTotalMortgage('10','1000','20000','24'))       // expected => 21042.09
+console.log(calculateTotalMortgage('10','20000','20000','24'))        // expected => 0
+console.log(calculateTotalMortgage('10','0','10000','36'))       // expected => 11616.19
+console.log(calculateTotalMortgage('15','0','10000','36'))       // expected => 12479.52
 
 
 function sayHello() {
